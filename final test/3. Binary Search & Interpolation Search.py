92e9738d2b909277a3af3dd131023d935d1f8ee6
ahmadrezkifebrian28-cell <ahmadrezkifebrian28@gmail.com>
2025-12-11 16:12:40 +0800

    Create 3. Binary Search & Interpolation Search.py

---
+++ final test/3. Binary Search & Interpolation Search.py
@@ -0,0 +1,43 @@
+# binary search
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    
+    return -1
+
+data = [10, 20, 30, 40, 50]
+
+
+# interpolation search
+def interpolation_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high and x >= arr[low] and x <= arr[high]:
+        pos = low + ((x - arr[low]) * (high - low) // (arr[high] - arr[low]))
+
+        if arr[pos] == x:
+            return pos
+        elif arr[pos] < x:
+            low = pos + 1
+        else:
+            high = pos - 1
+
+    return -1
+
+data = [10, 20, 30, 40, 50]
+
+print(binary_search(data, 30))
+print(interpolation_search(data, 40))
+
+
