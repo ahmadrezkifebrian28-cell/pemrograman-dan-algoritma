36be114ca075da4ee30fbe2401fed80585109142
ahmadrezkifebrian28-cell <ahmadrezkifebrian28@gmail.com>
2025-12-11 16:07:37 +0800

    Create 1. Contoh Tipe Data Array.py

---
+++ final test/1. Contoh Tipe Data Array.py
@@ -0,0 +1,29 @@
+# Array 1 dimensi (list biasa)
+angka = [10, 20, 30, 40, 50]
+
+
+# Array 2 dimensi (list dalam list)
+matriks = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+
+
+# Array 3 dimensi (list berlapis)
+kubus = [
+    [
+        [1, 2], 
+        [3, 4]
+    ],
+    [
+        [5, 6], 
+        [7, 8]
+    ]
+]
+print(kubus)
+print(matriks)
+print(angka)
+
+
