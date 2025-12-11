1fcd89c29e868075aecfa7f7d5ff4c5d148279ed
ahmadrezkifebrian28-cell <ahmadrezkifebrian28@gmail.com>
2025-12-11 16:14:28 +0800

    Create bubble sort.py

---
+++ final test/4. Algoritma sorting/bubble sort.py
@@ -0,0 +1,23 @@
+# bubble sort
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+data = [64, 25, 12, 22, 11]
+bubble_sort(data)
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+data = [64, 25, 12, 22, 11]
+
+print(data) # output bubble sort
